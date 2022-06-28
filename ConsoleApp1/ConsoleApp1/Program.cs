2c315f3a28e32a2942e429e182d4dd84724e301c
EshginCeferli <eshginij@code.edu.az>
2022-06-29 00:55:23 +0400

    Capsulation.Tasks

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //    MailPasword("cavid@code.edu.az", 12345);
+            //}
+            //static void MailPasword(string gmail, int password)
+            //{
+            //    if (gmail == "cavid@code.edu.az" && password == 12345)
+            //    {
+            //        Console.WriteLine("Girish olundu");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("mail ve ya password sehvdir");
+            //    }
+            Factorial(3);
+        }
+
+
+
+        static void Factorial(int num)
+        {
+            int fact = 1;
+            for (int i = 1; i <= num; i++)
+                
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine(fact);
+        }
+    }
+}
